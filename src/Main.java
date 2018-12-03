12cf583d55eb2a36f0e6ca9d7b8e1a4203eaab97
CLacaile <clementlacaile@gmail.com>
2018-12-03 11:45:15 +0100

    added format menu buttons

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,6 @@ public class Main {
         JACkModel model = new JACkModel();
         JACkView view = new JACkView();
         JACkController controller = new JACkController(model,view);
-
-        while(true) {
-            controller.updateTime();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 }
--- src/view/ClockBar.java
+++ src/view/ClockBar.java
@@ -8,12 +8,22 @@ public class ClockBar extends JMenuBar
 	private JMenu viewMenu = new JMenu("Vue");
 	private JMenu clockType = new JMenu("Clock");
 	private JMenu optionsMenu = new JMenu("Options");
+	private JMenu formatMenu = new JMenu("Format");
     private JMenuItem clock1 = new JMenuItem("Clock1");
 	private JMenuItem clock2 = new JMenuItem("Clock2");
 	private JMenuItem clock3 = new JMenuItem("Clock3");
+	private JMenuItem formatAMPM = new JMenuItem("AM/PM");
+	private JMenuItem format24h = new JMenuItem("24h");
 	private JCheckBoxMenuItem secondes = new JCheckBoxMenuItem("Secondes");
 
 	public ClockBar() {
+		formatMenu.add(formatAMPM);
+		formatMenu.add(format24h);
+		viewMenu.add(formatMenu);
+
+		viewMenu.add(formatMenu);
+		viewMenu.addSeparator();
+
 		clockType.add(clock1);
 		clockType.add(clock2);
 		clockType.add(clock3);
@@ -21,12 +31,6 @@ public class ClockBar extends JMenuBar
 		
 		viewMenu.addSeparator();
 		viewMenu.add(secondes);
-		/*secondes.addItemListener(new ItemListener() {
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				
-			}
-		  });*/
 		
 		this.add(viewMenu);
 		
