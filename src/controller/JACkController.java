983a4fbb0595933d37eecc6e1a81bb3087464955
Alexandre Ajinça <a.ajinca@laposte.net>
2018-12-04 16:27:07 +0100

    Add files via upload
    added the gestion of a 3rd format for English Clock

--- src/controller/JACkController.java
+++ src/controller/JACkController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.AbstractButton;
 
 import model.JACkModel;
 import view.JACkView;
@@ -9,7 +13,7 @@ import view.JACkView;
 public class JACkController {
     private JACkView view;
     private JACkModel model;
-    private int format;
+    private static int format;
     private Timer controlTimer;
 
     public JACkController(JACkModel theModel, JACkView theView) {
@@ -20,7 +24,39 @@ public class JACkController {
         this.updateTime();
     }
 
-    public void updateTime() {
+    private JACkView getView() {
+		return view;
+	}
+
+	private void setView(JACkView view) {
+		this.view = view;
+	}
+
+	private JACkModel getModel() {
+		return model;
+	}
+
+	private void setModel(JACkModel model) {
+		this.model = model;
+	}
+
+	private int getFormat() {
+		return format;
+	}
+
+	public static void setFormat(int f) {
+		format = f;
+	}
+
+	private Timer getControlTimer() {
+		return controlTimer;
+	}
+
+	private void setControlTimer(Timer controlTimer) {
+		this.controlTimer = controlTimer;
+	}
+
+	public void updateTime() {
         controlTimer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -33,8 +69,25 @@ public class JACkController {
                     view.displayTime(model.getClock().toString());
                     System.out.println(model.getClock().toString());
                 }
+                else if(format == 3) {
+                    view.displayTime(model.getClock().toStringEnglish());
+                    System.out.println(model.getClock().toStringEnglish());
+                }
             }
         }, 0, 1000);
+    }
+    
+    public class ShowSecondListener implements ActionListener {
+    	@Override
+    	public void actionPerformed(ActionEvent e) {
+    		AbstractButton second = (AbstractButton) e.getSource();
+    		boolean selected = second.getModel().isSelected();
+    		if(selected) {
+    			JACkController.setFormat(1);
+    		}
+    		else
+    			JACkController.setFormat(2);
+    	}
 
     }
 }
